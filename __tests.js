b32db2a3c987ce5bb872905252438cfe7c876707
Adam <adamoctav@gmail.com>
2021-04-09 22:43:11 +0300

    update resolve reject pattern and example

--- __tests.js
+++ __tests.js
@@ -1,23 +1,19 @@
-const {Lbry} = require('./lbry');
-const {launchSDK} = require('./start');
+const { Lbry } = require('./lbry');
+const { launchSDK } = require('./start');
 
 console.log('__test start')
-const statusCheck = ()=> Lbry.status().then(status=>{
+const statusCheck = () => Lbry.status().then(status => {
     console.log(`isRunning ${status.is_running}`)
     console.log(status);
 })
-const searchClaims = ()=> Lbry.claim_list().then(claims=>{
+const searchClaims = () => Lbry.claim_list().then(claims => {
     console.log(claims);
 })
-launchSDK().then(isSDKRunning=>{
+
+launchSDK().then(isSDKRunning => {
     console.log(`__test ${isSDKRunning}`);
     statusCheck();
     searchClaims();
-}).catch(isSDKRunning=>{
-    if(isSDKRunning === true){
-        statusCheck();
-        searchClaims();
-    }else{
-        console.error(isSDKRunning);
-    }
+}).catch(error => {
+    console.error(error);
 })
--- start.js
+++ start.js
@@ -3,9 +3,10 @@ const findProcess = require("find-process");
 const SDK = require("./sdk");
 
 const launchSDK = () => {
-
+  
   return new Promise((resolve, reject) => {
-    // Determine if the LBRY SDK is already running, or if it needs to be started
+    try {
+          // Determine if the LBRY SDK is already running, or if it needs to be started
     // This allows you to run the sdk binary separately and have your app connect to it
     const processListArgs = process.platform === "win32" ? "lbrynet" : "lbrynet start";
     findProcess("name", processListArgs).then(processList => {
@@ -16,11 +17,13 @@ const launchSDK = () => {
           sdk = null;
         });
         sdk.launch();
-        resolve(isSDKRunning);
-      } else {
-        reject(isSDKRunning)
       }
+      resolve(isSDKRunning);
     });
+    } catch (error) {
+      reject(error)
+    }
+
   })
 }
 
