f46f910a2783bc543bb8ba78dd8ffed61570751e
Adam <adamoctav@gmail.com>
2021-04-10 18:59:50 +0300

    fix path and sdk start issues

--- sdk.js
+++ sdk.js
@@ -3,17 +3,17 @@ const { spawn, execSync } = require("child_process");
 
 class SDK {
   constructor() {
-    this.path = process.env.LBRY_DAEMON || path.join(__dirname, "/dist/sdk/lbrynet");
+    this.path = process.env.LBRY_DAEMON || path.join(__dirname, "/dist/lbrynet");
     this.handlers = [];
     this.subprocess = undefined;
   }
 
   launch() {
-    this.subprocess = spawn(this.path, ["start"]);
-    this.subprocess.stdout.on("data", data => console.log(`SDK: ${data}`));
-    this.subprocess.stderr.on("data", data => console.error(`SDK: ${data}`));
+    this.subprocess = spawn(this.path, ["start", "--api", "127.0.0.1:5279", "--streaming-server", "127.0.0.1:5280"]);
+    this.subprocess.stdout.on("data", data => console.log(`SDK data: ${data}`));
+    this.subprocess.stderr.on("data", data => console.log(`SDK error: ${data}`));
     this.subprocess.on("exit", () => this.fire("exit"));
-    this.subprocess.on("error", error => console.error(`SDK error: ${error}`));
+    this.subprocess.on("error", error => console.log(`SDK error: ${error}`));
   }
 
   quit() {
